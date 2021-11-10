63b267df9e888c32a778286f473a33c681ba88e7
Nizam Uddin Babu <nizamcse.seu@gmail.com>
2021-11-10 08:45:58 +0600

    addUser POST API is Done

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const cors =require('cors');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const port = process.env.PORT || 5000;
+
 
 
 const user= process.env.DB_USER;
 const pass =process.env.DB_PASS
+
+//Middleware
+
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${user}:${pass}@cluster0.tgh4y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,7 +25,16 @@ const run =  async ()=>{
 
     try{
        await client.connect();
-       console.log("mongo");
+   
+       const database =client.db('shoeLace');
+       const userCollection = database.collection('users');
+    //    const ItemsCollection = database.collection('items');
+
+        app.post('/addUser',async(req,res)=>{
+            const userData = req.body;
+            const result = await userCollection.insertOne(userData)
+            res.json(result)
+        })
 
 
        app.get('/',(req,res)=>{
@@ -34,9 +52,6 @@ run().catch(console.dir)
 
 
 
-app.get('/',(req,res)=>{
-    res.send("Hello world")
-})
 
 app.listen(port,()=>{
     console.log("listening ");
